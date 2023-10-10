110ac07baca4c5fba2ed2ead372cdcdc0d963d87
Michael Lei <michaelleie0ca@gmail.com>
2023-10-09 20:33:11 -0700

    Change sound length to 5

--- app/api/generate/sound/route.ts
+++ app/api/generate/sound/route.ts
@@ -2,9 +2,6 @@ import { generateSound } from "@/lib/generateSound";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  // get the file from the request
-  //   const data = await req.formData();
-  //   const image = data.get("image") as File;
   const { classification } = await req.json();
 
   const output = await generateSound(classification);
--- lib/generateSound.ts
+++ lib/generateSound.ts
@@ -7,7 +7,7 @@ const replicate = new Replicate({
 const model =
   "sepal/audiogen:154b3e5141493cb1b8cec976d9aa90f2b691137e39ad906d2421b74c2a8c52b8";
 
-export const generateSound = async (prompt: string) =>
+export const generateSound = async (prompt: string, length = 5) =>
   await replicate.run(model, {
-    input: { prompt },
+    input: { prompt, length },
   });
